bf468fef36b4cd762bd29ad5ccb34813d938fc7a
cvsubmit <cvsubmit@outlook.com>
2020-10-08 21:23:52 +0800

    update watch script

--- lib/utils/extract_tpelog.py
+++ lib/utils/extract_tpelog.py
@@ -32,26 +32,33 @@ def collect_results(args):
     count = 0 # total numbers
 
     for line in lines:
-        if not line.startswith('penalty_k'):
+        if not 'penalty_k:' in line:
             pass
         else:
-     #       print(line)
             count += 1
             temp0, temp1, temp2, temp3, temp4, temp5, temp6 = line.split(',')
+            #print(temp6.split(': ')[-1])
+            #exit()
             penalty_k.append(float(temp0.split(': ')[-1]))
             scale_lr.append(float(temp1.split(': ')[-1]))
             wi.append(float(temp2.split(': ')[-1]))
             sz.append(float(temp3.split(': ')[-1]))
             bz.append(float(temp4.split(': ')[-1]))
             ratio.append(float(temp5.split(': ')[-1]))
-            eao.append(float(temp6.split(': ')[-1]))
+            try:
+                eao.append(float(temp6.split(': ')[-1].split('==')[0]))
+            except:
+                eao.append(float(temp6.split(': ')[-1].split('Result')[0]))
+                #print(line)
+                #print(temp6.split(': ')[-1])
+                #exit()
 
     # find max
     eao = np.array(eao)
     max_idx = np.argmax(eao)
     max_eao = eao[max_idx]
     print('{} params group  have been tested'.format(count))
-    print('penalty_k: {:.4f}, scale_lr: {:.4f}, wi: {:.4f}, small_sz: {}, big_sz: {}, auc: {}'.format(penalty_k[max_idx], scale_lr[max_idx], wi[max_idx], sz[max_idx], bz[max_idx], max_eao))
+    print('penalty_k: {:.4f}, scale_lr: {:.4f}, wi: {:.4f}, ratio: {:.4f}, small_sz: {}, big_sz: {}, auc: {}'.format(penalty_k[max_idx], scale_lr[max_idx], wi[max_idx], ratio[max_idx], sz[max_idx], bz[max_idx], max_eao))
 
 
 if __name__ == '__main__':
